b09094b307836e3319b57e3d5ed2addb352380aa
Poketan567 <poketan567@gmail.com>
2026-09-18 18:50:51 -0400

    Latest version of AprilTag Follower, currently doesn't work.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TeleOp_AprilTag_Follower.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TeleOp_AprilTag_Follower.java
@@ -53,19 +53,26 @@ public class TeleOp_AprilTag_Follower extends OpMode {
     @Override
     public void loop() {
         LLResult result = limelight.getLatestResult();
+        robot.initBulkCache();
+        robot.updateBulkCache();
+        teleop.drivetrainLogic(false, false, 100);
         for (LLResultTypes.FiducialResult fiducial : result.getFiducialResults()) {
             if (targetID == 0) {
                 targetID = fiducial.getFiducialId();
             } if (fiducial.getFiducialId() == targetID) {
+                tm.print(String.valueOf(fiducial.getTargetXPixels()));
                 if (gamepad == null) { tm.print("Gamepad returning null!"); }
-                else if (fiducial.getTargetXPixels() <= -0.5) { gamepad.left_stick_x = -0.25f; }
-                else if (fiducial.getTargetXPixels() >= 0.5) { gamepad.left_stick_x = 0.25f; }
-                else { gamepad.left_stick_x = 0f; }
+                else if (fiducial.getTargetXPixels() <= -7000) { gamepad.left_stick_x = -0.25f; }
+                else if (fiducial.getTargetXPixels() >= 7000) { gamepad.left_stick_x = 0.25f; }
+                else {
+                    gamepad.left_stick_x = 0.1f;
+
+                }
             }
         }
-        robot.initBulkCache();
-        robot.updateBulkCache();
-        teleop.drivetrainLogic(false, false, 200);
+        tm.print(String.valueOf(targetID));
+        tm.print(String.valueOf(gamepad.left_stick_x));
+        tm.print(String.valueOf(gamepad));
         tm.update();
 
 

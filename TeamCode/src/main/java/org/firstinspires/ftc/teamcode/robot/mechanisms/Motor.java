0828adb434a5a66f97553496da84da5459af5fc9
Poketan567 <poketan567@gmail.com>
2026-09-25 16:13:56 -0400

    Made a simple Motor class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/mechanisms/Motor.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.robot.mechanisms;
+
+import androidx.annotation.Nullable;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.TelemetryUtils;
+import org.firstinspires.ftc.teamcode.robot.HardwareInitializer;
+
+public class Motor {
+    private final @Nullable DcMotorEx motor;
+
+    public Motor(HardwareMap hardwareMap, TelemetryUtils tm) {
+        motor = HardwareInitializer.init(hardwareMap, DcMotorEx.class, "Motor");
+        if (motor == null) { tm.warn(TelemetryUtils.ErrorLevel.HIGH, "Motor is returning null! Please check if its plugged in."); }
+    }
+
+    /**
+     * Makes the motor spin
+     */
+    public void spin() { if (motor != null) { motor.setPower(1); } }
+
+    /**
+     *  Makes the motor spin
+     * @param power Changes the power of the motor (-1 to 1)
+     */
+    public void spin(double power) { if (motor != null) { motor.setPower(power); } }
+
+    /**
+     * Stops the motor
+     */
+    public void stop() { if (motor != null) { motor.setPower(0); } }
+}
